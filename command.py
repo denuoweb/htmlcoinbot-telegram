52ba43d5a63561660f3c5fcc8181f30a3349e2b2
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-02-16 23:36:32 -0800

    Update command.py

--- command.py
+++ command.py
@@ -1,5 +1,3 @@
--*- coding: utf-8 -*-
-
 import json
 import codecs
 import requests
@@ -19,17 +17,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def commands(bot, update):
 	user = update.message.from_user.username 
-	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of htmlcoin to utilise \n <address> = htmlcoin address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
+	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /commands , /deposit , /tip , /withdraw , or /balance .\n \n Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of htmlcoin to utilise \n <address> = htmlcoin address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
 
 def help(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , /price , /marketcap or /balance")
+	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /commands , /deposit , /tip , /withdraw , or /balance ")
 
 def deposit(bot, update):
 	user = update.message.from_user.username
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
-		address = "/home/mobileapi/HTMLCOIN/src/htmlcoind"
+		address = "/home/mobileapi/HTMLCOIN/src/htmlcoin-cli"
 		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
@@ -48,7 +46,7 @@ def tip(bot,update):
 		elif "@" in target:
 			target = target[1:]
 			user = update.message.from_user.username 
-			core = "/home/mobileapi/HTMLCOIN/src/htmlcoind"
+			core = "/home/mobileapi/HTMLCOIN/src/htmlcoin-cli"
 			result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 			balance = float((result.stdout.strip()).decode("utf-8"))
 			amount = float(amount)
@@ -77,7 +75,7 @@ def balance(bot,update):
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
-		core = "/home/mobileapi/HTMLCOIN/src/htmlcoind"
+		core = "/home/mobileapi/HTMLCOIN/src/htmlcoin-cli"
 		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		balance  = float(clean)
@@ -86,21 +84,7 @@ def balance(bot,update):
 		balance =  str(round(balance,3))
 		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} HTML ≈  ${2}".format(user,balance,fiat_balance))
 
-def price(bot,update):
-	quote_page = requests.get('https://www.worldcoinindex.com/coin/htmlcoin')
-	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
-	soup = BeautifulSoup(quote_page.content, 'html.parser', parse_only=strainer)
-	name_box = soup.find('div', attrs={'class':'col-md-6 col-xs-6 coinprice'})
-	name = name_box.text.replace("\n","")
-	price = re.sub(r'\n\s*\n', r'\n\n', name.strip(), flags=re.M)
-	fiat = soup.find('span', attrs={'class': ''})
-	kkz = fiat.text.replace("\n","")
-	percent = re.sub(r'\n\s*\n', r'\n\n', kkz.strip(), flags=re.M)
-	quote_page = requests.get('https://bleutrade.com/api/v2/public/getticker?market=HTML_BTC')
-	soup = BeautifulSoup(quote_page.content, 'html.parser').text
-	btc = soup[80:]
-	sats = btc[:-2]
-	bot.send_message(chat_id=update.message.chat_id, text="Htmlcoin is valued at {0} Δ {1} ≈ {2}".format(price,percent,sats) + " ฿")
++ " ฿")
 
 def withdraw(bot,update):
 	user = update.message.from_user.username
@@ -112,7 +96,7 @@ def withdraw(bot,update):
 		address = ''.join(str(e) for e in address)
 		target = target.replace(target[:35], '')
 		amount = float(target)
-		core = "/home/mobileapi/HTMLCOIN/src/htmlcoind"
+		core = "/home/mobileapi/HTMLCOIN/src/htmlcoin-cli"
 		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		balance = float(clean)
@@ -121,40 +105,16 @@ def withdraw(bot,update):
 		else:
 			amount = str(amount)
 			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
-			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} HTML" .format(user,address,amount))
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrawn to address: {1} Amount: {2} HTML - gas" .format(user,address,amount))
 
-def hi(bot,update):
-	user = update.message.from_user.username
-	bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, how are you doing today?".format(user))
-
-def moon(bot,update):
-  bot.send_message(chat_id=update.message.chat_id, text="Moon mission inbound!")
-
-def marketcap(bot,update):
-	quote_page = requests.get('https://www.worldcoinindex.com/coin/htmlcoin')
-	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
-	soup = BeautifulSoup(quote_page.content, 'html.parser', parse_only=strainer)
-	name_box = soup.find('div', attrs={'class':'col-md-6 col-xs-6 coin-marketcap'})
-	name = name_box.text.replace("\n","")
-	mc = re.sub(r'\n\s*\n', r'\n\n', name.strip(), flags=re.M)
-	bot.send_message(chat_id=update.message.chat_id, text="The current market cap of Htmlcoin is valued at {0}".format(mc))
-
+			
 from telegram.ext import CommandHandler
 
 commands_handler = CommandHandler('commands', commands)
 dispatcher.add_handler(commands_handler)
 
-moon_handler = CommandHandler('moon', moon)
-dispatcher.add_handler(moon_handler)
-
-hi_handler = CommandHandler('hi', hi)
-dispatcher.add_handler(hi_handler)
-
 withdraw_handler = CommandHandler('withdraw', withdraw)
 dispatcher.add_handler(withdraw_handler)
-
-marketcap_handler = CommandHandler('marketcap', marketcap)
-dispatcher.add_handler(marketcap_handler)
 
 deposit_handler = CommandHandler('deposit', deposit)
 dispatcher.add_handler(deposit_handler)
